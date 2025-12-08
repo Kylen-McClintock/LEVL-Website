783b566758e6e21bbfdb22e0630d8e12ab11f22e
Kylen McClintock <kylenmcclintock@MacBook-Air.local>
2025-12-08 12:43:35 -0700

    Fix syntax error in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,34 @@
 import { Container } from "@/components/ui/Container";
 import { BentoGrid } from "@/components/ui/BentoGrid";
 import { Navbar } from "@/components/sections/Navbar";
-<BentoGrid>
-  <Hero />
-  <TaglineBand />
-  <BenefitTimeline />
-  <SocialProof />
-  <DeepCellAppCard />
-  <EmailCapture />
-</BentoGrid>
+import { Hero } from "@/components/sections/Hero";
+import { TaglineBand } from "@/components/sections/TaglineBand";
+import { BenefitTimeline } from "@/components/sections/BenefitTimeline";
+import { DeepCellAppCard } from "@/components/sections/DeepCellAppCard";
+import { EmailCapture } from "@/components/sections/EmailCapture";
+import { SocialProof } from "@/components/sections/SocialProof";
+import { ScienceExplorer } from "@/components/sections/ScienceExplorer";
+import { ScienceOfSleep } from "@/components/sections/ScienceOfSleep";
+import { BrandMission } from "@/components/sections/BrandMission";
+import { Footer } from "@/components/sections/Footer";
 
-{/* Science Explorer is full width outside of the main bento grid wrapper to allow custom grid inside */ }
+export default function Home() {
+  return (
+    <main className="min-h-screen pb-20 overflow-x-hidden">
+      <Navbar />
+
+      <Container className="pt-24 md:pt-32 space-y-4 md:space-y-6">
+
+        <BentoGrid>
+          <Hero />
+          <TaglineBand />
+          <BenefitTimeline />
+          <SocialProof />
+          <DeepCellAppCard />
+          <EmailCapture />
+        </BentoGrid>
+
+        {/* Science Explorer is full width outside of the main bento grid wrapper to allow custom grid inside */}
         <div id="science">
           <ScienceExplorer />
         </div>
@@ -20,9 +38,9 @@ import { Navbar } from "@/components/sections/Navbar";
           <BrandMission />
         </BentoGrid>
 
-      </Container >
+      </Container>
 
-  <Footer />
-    </main >
+      <Footer />
+    </main>
   );
 }
